6a9c75e1086b4cce634b15b760ff1066375a4c00
Hanna <hanna.tselesh@yahoo.com>
2023-05-06 22:46:56 -0400

    new git

--- src/step1/FirstClass.java
+++ src/step1/FirstClass.java
@@ -2,6 +2,6 @@ package step1;
 
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        System.out.println("Hello Git!");
     }
 }
